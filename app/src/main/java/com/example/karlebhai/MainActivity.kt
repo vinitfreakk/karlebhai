1f395345968a0b88fbcf7042f0d3c5461b8c8a57
vinit <vinithanabar20@gmail.com>
2023-06-27 19:11:37 +0530

    made the app structure and added navController

---
+++ app/src/main/java/com/example/karlebhai/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.karlebhai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    lateinit var navController:NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+         navController = findNavController(R.id.navigator) // got the navigation controller here
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onNavigateUp()
+    }
+}
